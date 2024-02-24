d5bae84fe071a59990b442d086a632e27dcb59ad
KIMSIHUN <tlgns14@nate.com>
2024-02-24 19:34:23 +0900

    🎨 모델 Comment에 필드 및 태그 변경 사항 추가

--- src/models/comment.go
+++ src/models/comment.go
@@ -4,13 +4,13 @@ import (
 	"time"
 )
 type Comment struct {
-    ID        int           `gorm:"primaryKey"`
-    MovieId   int
-    CreatedAt time.Time
-    UpdatedAt time.Time
-    DeletedAt *time.Time `gorm:"index"`
-    UserNo    int
-    Comment   string
+    ID        int   `json:"cateno" gorm:"primaryKey"`
+    MovieId   int   `json:"movieid" gorm:"column:movieId;not null"`
+    CreatedAt time.Time     `json:"created_at" gorm:"column:createdAt;type:datetime;default:CURRENT_TIMESTAMP(6)"`
+    UpdatedAt time.Time     `json:"updated_at" gorm:"column:updatedAt;type:datetime;default:CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6)"`
+    DeletedAt *time.Time    `json:"deleted_at" gorm:"column:deletedAt;type:datetime;default:NULL;"`
+    UserNo    int  `json:"userno" gorm:"column:userno;not null"`
+    Comment   string `json:"comment" gorm:"column:comment;not null;"`
 
     Movie Movie `gorm:"foreignKey:MovieId;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
     User  User  `gorm:"foreignKey:UserNo;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
