6b9ffe006df6c18002fca7ba3f035683292f808b
KIMSIHUN <tlgns14@nate.com>
2024-06-15 14:28:17 +0900

    🎨

--- src/controller/auth.controller.go
+++ src/controller/auth.controller.go
@@ -1,7 +1,10 @@
 package controller
 
 import (
+	"moview/src/models"
 	"moview/src/repository"
+
+	"github.com/gofiber/fiber/v2"
 )
 
 type AuthController struct {
@@ -10,4 +13,17 @@ type AuthController struct {
 
 func NewAuthController(authRepository repository.AuthRepository) *AuthController {
 	return &AuthController{AuthRepository: authRepository}
+}
+
+func (ac *AuthController) LogIn(c *fiber.Ctx) error {
+	user := new(models.User)
+	if err := c.BodyParser(user); err != nil {
+		return err
+	}
+
+	u, err := ac.AuthRepository.LogIn(user)
+	if err != nil {
+		return err
+	}
+	return c.JSON(u)
 }
